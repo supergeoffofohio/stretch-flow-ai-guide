2d1b1f171a9670a3b7ff97872a713175c231e277
Geoffrey Belanger <geoffreybelanger@Geoffreys-MacBook-Pro.local>
2025-06-17 08:28:20 -0400

    Initialize Xcode project

---
+++ StretchFlow/StretchFlow/StretchFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchFlowApp.swift
+//  StretchFlow
+//
+//  Created by Geoffrey Belanger on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct StretchFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StretchFlow/StretchFlowTests/StretchFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  StretchFlowTests.swift
+//  StretchFlowTests
+//
+//  Created by Geoffrey Belanger on 6/17/25.
+//
+
+import Testing
+@testable import StretchFlow
+
+struct StretchFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
